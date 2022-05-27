c64bd5e7f99dde0e2cca32e69f6d871e09b402f9
= <=>
2022-05-27 09:58:41 +0200

    Chef part ready

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 
+import { images } from '../../constants';
 import './Chef.css';
+import SubHeading from './../../components/SubHeading/SubHeading';
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+   <div className='app__bg app__wrapper section__padding'>
+      <div className="app__wrapper_img app__wrapper_img-reverse">
+         <img src={images.chef} alt="chef" />
+      </div>
+      <div className="app__wrapper_info">
+         <SubHeading title="Chef's word" />
+         <h1 className="headtext__cormorant">What we believe in</h1>
+
+         <div className="app__chef-content">
+            <div className="app__chef-content_quote">
+               <img src={images.quote} alt="quote" />
+               <p className='p__opensans'>Lorem ipsum dolor sit amet, </p>
+            </div>
+            <p className='p__opensans'>consectetur adipisicing elit. Fugiat fugit ut sint placeat atque tempore, consequatur consequuntur beatae assumenda repellendus quia voluptates, magnam sequi sapiente, odio nam nihil nobis esse.</p>
+         </div>
+
+         <div className="app__chef-sign">
+            <p>Liu Kang</p>
+            <p className='p__opensans'>Chef & Founder</p>
+            <img src={images.sign} alt="sign" />
+         </div>
+
+      </div>
+   </div>
 );
 
 export default Chef;
